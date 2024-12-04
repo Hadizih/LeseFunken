b4a0519a204d41675d30fca045125bb05dea6129
Dominic Strybny <Dominic.Strybny@gmail.com>
2024-12-04 20:38:04 +0100

    animation funktioniert

--- static/script.js
+++ static/script.js
@@ -1,6 +1,49 @@
-radioBtnFormat = document.querySelectorAll('input[name="radioBtnFormat"]');
-radioBtnFlow = document.querySelectorAll('input[name="radioBtnFlow"]');
-textElement = document.getElementById('text');
+const radioBtnFormat = document.querySelectorAll('input[name="radioBtnFormat"]');
+const radioBtnFlow = document.querySelectorAll('input[name="radioBtnFlow"]');
+const textElement = document.getElementById('text');
+const btnStart = document.getElementById('btnStart');
+const btnPause = document.getElementById('btnPause');
+const textField = document.getElementById('text');
+let animationPaused = false;
+let index = 0;
+let words = [];
+let timeoutId;
+
+const start = () => {
+  btnPause.hidden = false;
+  if (index === 0) {
+      words = textField.textContent.split(" ");
+  }
+
+  animationPaused = false; 
+
+  const highlightNextWord = () => {
+      if (animationPaused) return; 
+
+      textField.innerHTML = words
+          .map((word, i) => {
+              if (i === index) {
+                  return `<span class="highlight">${word}</span>`;
+              }
+              return word;
+          })
+          .join(" ");
+
+      index++; 
+
+      if (index < words.length) {
+          timeoutId = setTimeout(highlightNextWord, 1500); 
+      }
+  };
+
+  highlightNextWord();
+};
+
+const pause = () => {
+  animationPaused = true; 
+  clearTimeout(timeoutId);
+  index -= 1; 
+};
 
 const changeTextSettings = () => {
   const selectedFormat = document.querySelector('input[name="radioBtnFormat"]:checked').value;
